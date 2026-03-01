ba0080352306b4e6c0a2b4787010f454289afba8
developerhizol <kostatrofimcev11@gmail.com>
2026-03-01 11:12:11 +0300

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public_html')));
 
 app.get('/api/config', (req, res) => {
     res.json({
